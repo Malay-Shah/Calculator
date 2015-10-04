1f985a8bbb2243c9631301b59b31fb385d29b436
Malay <malay159>
2015-10-04 11:59:12 -0400

    Basic calculator templated created, with Names and Ids. Button style changed. Changes to the style and some buttons. Margins around the buttons. Declared the variables. Major coding done. Needs enhancements for doing multiple commands.

--- app/src/main/java/com/random/malay/calculator/MainActivity.java
+++ app/src/main/java/com/random/malay/calculator/MainActivity.java
@@ -5,12 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    Button bC, bPerecent, bPosNeg, bDivide,bMultiply,bSub, bAdd, bEqual, bDot, bDelete,b0 ,b1,b2,b3,b4,b5,b6,b7,b8,b9;
+    Button bC, bPercent, bPosNeg, bDivide,bMultiply,bSub, bAdd, bEqual, bDot, bDelete,b0 ,b1,b2,b3,b4,b5,b6,b7,b8,b9;
     EditText etMain, etSub;
     String optr;
     double reminder;
@@ -21,7 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         bC = (Button) findViewById(R.id.bC);
-        bPerecent = (Button) findViewById(R.id.bPercent);
+        bPercent = (Button) findViewById(R.id.bPercent);
         bPosNeg = (Button) findViewById(R.id.bPosNeg);
         bDivide = (Button) findViewById(R.id.bDivide);
         bMultiply = (Button) findViewById(R.id.bMultiply);
@@ -44,11 +43,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         etMain = (EditText) findViewById(R.id.tvMainText);
         etSub = (EditText) findViewById(R.id.tvSubText);
+        etMain.setText("");
+        etSub.setText("");
 
         try{
 
             bC.setOnClickListener(this);
-            bPerecent.setOnClickListener(this);
+            bPercent.setOnClickListener(this);
             bPosNeg.setOnClickListener(this);
             bDivide.setOnClickListener(this);
             bMultiply.setOnClickListener(this);
@@ -74,42 +75,164 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
-
+    double mem=0,mem2=0;
     @Override
     public void onClick(View args){
-        double mem;
+
+        double ran;
+        double result;
+        String input;
+
         switch(args.getId()){
             case R.id.bC:
                 if(!(etMain.getText().toString().equals(""))) {
                     etMain.setText("");
                     optr = "none";
+                    mem = 0;
+                    mem2 = 0;
                 }
                 break;
             case R.id.bPercent:
                 if(reminder!=0){
-                     mem = reminder * (Double.valueOf(etMain.toString()));
-
+                     ran = mem * (Double.parseDouble(etMain.getText().toString()))/100;
+                     etMain.setText(convertDoubleToString(ran));
+                }else{
+                    etMain.setText("0");
                 }
                 break;
             case R.id.bPosNeg:
+                if(!(etMain.getText().toString().equals(""))) {
+                    ran = (-1 * Double.valueOf(etMain.getText().toString()));
+                    etMain.setText(convertDoubleToString(ran));
+                }
                 break;
             case R.id.bDivide:
+                optr = "/";
+                if(mem ==0) {
+                    mem = Double.valueOf(etMain.getText().toString());
+                    etMain.setText("");
+                }else{
+                    mem2 = Double.valueOf(etMain.getText().toString());
+                    mem = mem / mem2;
+                    etMain.setText("");
+                }
                 break;
             case R.id.bMultiply:
+                optr = "*";
+                if(mem ==0) {
+                    mem = Double.valueOf(etMain.getText().toString());
+                    etMain.setText("");
+                }else{
+                    mem2 = Double.valueOf(etMain.getText().toString());
+                    mem = mem * mem2;
+                    etMain.setText("");
+                }
                 break;
             case R.id.bSub:
+                optr = "-";
+                if(mem ==0) {
+                    mem = Double.valueOf(etMain.getText().toString());
+                    etMain.setText("");
+                }else{
+                    mem2 = Double.valueOf(etMain.getText().toString());
+                    mem = mem - mem2;
+                    etMain.setText("");
+                }
                 break;
             case R.id.bAdd:
+                optr = "+";
+                if(mem ==0) {
+                    mem = Double.valueOf(etMain.getText().toString());
+                    etMain.setText("");
+                }else{
+                    mem2 = Double.valueOf(etMain.getText().toString());
+                    mem = mem + mem2;
+                    etMain.setText("");
+                }
                 break;
             case R.id.bDot:
+                input = etMain.getText().toString();
+                etMain.setText(input + ".");
                 break;
             case R.id.bDelete:
                 break;
             case R.id.bEqual:
+                switch (optr){
+                    case "none":
+                        break;
+                    case "/":
+                        mem2 = Double.valueOf(etMain.getText().toString());
+                        mem = mem/mem2;
+                        etMain.setText(convertDoubleToString(mem));
+                        break;
+                    case "*":
+                        mem2 = Double.valueOf(etMain.getText().toString());
+                        mem = mem*mem2;
+                        etMain.setText(convertDoubleToString(mem));
+                        break;
+                    case "-":
+                        mem2 = Double.valueOf(etMain.getText().toString());
+                        mem = mem-mem2;
+                        etMain.setText(convertDoubleToString(mem));
+                        break;
+                    case "+":
+                        mem2 = Double.valueOf(etMain.getText().toString());
+                        mem = mem+mem2;
+                        etMain.setText(convertDoubleToString(mem));
+                        break;
+                    default:
+                        break;
+                }
+
                 break;
             case R.id.b0:
+                input = etMain.getText().toString();
+                etMain.setText(input + "0");
+                break;
+            case R.id.b1:
+                input = etMain.getText().toString();
+                etMain.setText(input + "1");
+                break;
+            case R.id.b2:
+                input = etMain.getText().toString();
+                etMain.setText(input + "2");
+                break;
+            case R.id.b3:
+                input = etMain.getText().toString();
+                etMain.setText(input + "3");
+                break;
+            case R.id.b4:
+                input = etMain.getText().toString();
+                etMain.setText(input + "4");
+                break;
+            case R.id.b5:
+                input = etMain.getText().toString();
+                etMain.setText(input + "5");
+                break;
+            case R.id.b6:
+                input = etMain.getText().toString();
+                etMain.setText(input + "6");
+                break;
+            case R.id.b7:
+                input = etMain.getText().toString();
+                etMain.setText(input + "7");
+                break;
+            case R.id.b8:
+                input = etMain.getText().toString();
+                etMain.setText(input + "8");
+                break;
+            case R.id.b9:
+                input = etMain.getText().toString();
+                etMain.setText(input + "9");
                 break;
 
+
         }
+    }
+
+    public String convertDoubleToString(double input){
+        String str;
+        str = String.valueOf(input);
+        return str;
     }
 }
